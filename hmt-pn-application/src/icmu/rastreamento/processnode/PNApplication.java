f8c0f090df1f380a778fb71d43ebc0848416d069
milliandrade <milliandrade@gmail.com>
2023-12-07 16:01:55 -0300

    adding jar files

--- hmt-pn-application/src/icmu/rastreamento/processnode/PNApplication.java
+++ hmt-pn-application/src/icmu/rastreamento/processnode/PNApplication.java
@@ -31,12 +31,39 @@ public class PNApplication extends ModelApplication {
 	public PNApplication() {
 		this.objectMapper = new ObjectMapper();
 		this.swap = new Swap(objectMapper);
-//		this.farm = new Farm();
+		// this.farm = new Farm();
 		this.farm = new Farm("HMT Farm System");
-//		this.farm.initializeFarm();
+		// this.farm.initializeFarm();
 	}
 
 	public static void main(String[] args) {
+		if (args.length > 0) {
+			for (int i = 0; i < args.length; i++) {
+				if (args[i] != null && !args[i].equalsIgnoreCase("")) {
+					// System.out.println(i + ":" + args[i]);
+					switch (i) {
+						case 0:// args[0] => data_base_address
+							Constants.setMYSQL_ADDRESS(args[i]);
+							break;
+						case 1:// args[1] => db_username
+							Constants.setUSERNAME(args[i]);
+							break;
+						case 2:// args[2] => db_password
+							Constants.setPASSWD(args[i]);
+							break;
+						case 3:// args[3] => gatway_ip_address
+							Constants.setGATEWAY_IP_ADDRESS(args[i]);
+							break;
+						case 4:// args[4] => gatway_ip
+							Constants.setGATEWAY_IP(args[i]);
+							break;
+						default:
+							break;
+					}
+				}
+			}
+
+		}
 		PNApplication pna = new PNApplication();
 		System.out.println("execução...");
 		pna.ApplicationBehavior();
@@ -68,14 +95,14 @@ public class PNApplication extends ModelApplication {
 
 	public void ApplicationBehavior() {
 		String uuid = Constants.GROUND_STATION_UUID;
-//		String uuid = "02222222-2222-2222-2222-222222222222";
-//		Farm farm = new Farm("Teste");
+		// String uuid = "02222222-2222-2222-2222-222222222222";
+		// Farm farm = new Farm("Teste");
 		boolean finish = false;
 		try {
 			int optionNotify = 0;
 			Ox tmpOx = null;
 			while (!finish) {
-//				System.out.flush();
+				// System.out.flush();
 				String consulta = "SELECT notify FROM farm WHERE name=\'" + this.getFarm().getName() + "\';";
 
 				ResultSet resultado = this.getFarm().getQuery().executeQuery(consulta);
@@ -83,69 +110,69 @@ public class PNApplication extends ModelApplication {
 					optionNotify = resultado.getInt("notify");
 				}
 				switch (optionNotify) {
-				case 0:
-					// Não houve alteração
-//					System.out.println("sem alterações...");
-					this.wait(1000000);
-					break;
-				case 1:
-					// Solicitação de rastreamento
-					System.out.println("solicitado o rastreamento...");
+					case 0:
+						// Não houve alteração
+						// System.out.println("sem alterações...");
+						this.wait(1000000);
+						break;
+					case 1:
+						// Solicitação de rastreamento
+						System.out.println("solicitado o rastreamento...");
 
-					consulta = "UPDATE farm SET notify = \'0\';";
-					this.getFarm().getQuery().executeUpdate(consulta);
+						consulta = "UPDATE farm SET notify = \'0\';";
+						this.getFarm().getQuery().executeUpdate(consulta);
 
-					this.sendUnicastMessage(new Vector<String>());
+						this.sendUnicastMessage(new Vector<String>());
 
-					break;
-				case 2:
-					// Inseriu um novo boi
-					System.out.println("boi inserido...");
-					this.getFarm().actualizeFarmFromBD();
-					
-					consulta = "UPDATE farm SET notify = \'0\';";
-					this.getFarm().getQuery().executeUpdate(consulta);
+						break;
+					case 2:
+						// Inseriu um novo boi
+						System.out.println("boi inserido...");
+						this.getFarm().actualizeFarmFromBD();
 
-					break;
-				case 3:
-					// TESTE ::: Inseriu um novo boi
-					System.out.println("adicionando graze...");
-					Graze tmp = new Graze();
-					this.farm.addGraze(tmp);
-					this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(0).getId());
-					this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(1).getId());
-					this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(2).getId());
-					this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(3).getId());
-					this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(4).getId());
-					tmp.setEnd(LocalDateTime.now());
-					consulta = tmp.updateGraze();
-//					this.farm.printOxen();
-					this.farm.getQuery().executeUpdate(consulta);
-					for (int i = 0; i < this.getFarm().getGrazesById(tmp.getId()).getPresentIDs().size(); i++) {
-						System.out.println(this.getFarm().getGrazesById(tmp.getId()).getPresentIDs().get(i));
-					}
-					String[] tmpP = tmp.insertPresents().split("\n");
-					for (int i = 0; i < tmpP.length; i++) {
-						consulta = tmpP[i];
+						consulta = "UPDATE farm SET notify = \'0\';";
+						this.getFarm().getQuery().executeUpdate(consulta);
+
+						break;
+					case 3:
+						// TESTE ::: Inseriu um novo boi
+						System.out.println("adicionando graze...");
+						Graze tmp = new Graze();
+						this.farm.addGraze(tmp);
+						this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(0).getId());
+						this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(1).getId());
+						this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(2).getId());
+						this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(3).getId());
+						this.farm.insertOxIntoGraze(tmp.getId(), this.getFarm().getOxen().get(4).getId());
+						tmp.setEnd(LocalDateTime.now());
+						consulta = tmp.updateGraze();
+						// this.farm.printOxen();
 						this.farm.getQuery().executeUpdate(consulta);
-					}
+						for (int i = 0; i < this.getFarm().getGrazesById(tmp.getId()).getPresentIDs().size(); i++) {
+							System.out.println(this.getFarm().getGrazesById(tmp.getId()).getPresentIDs().get(i));
+						}
+						String[] tmpP = tmp.insertPresents().split("\n");
+						for (int i = 0; i < tmpP.length; i++) {
+							consulta = tmpP[i];
+							this.farm.getQuery().executeUpdate(consulta);
+						}
 
-					String[] tmpA = tmp.insertAbsentsToBD().split("\n");
-					for (int i = 0; i < tmpA.length; i++) {
-						consulta = tmpA[i];
-						System.out.println("absent ox to BD:: " + consulta);
-						this.farm.getQuery().executeUpdate(consulta);
-					}
+						String[] tmpA = tmp.insertAbsentsToBD().split("\n");
+						for (int i = 0; i < tmpA.length; i++) {
+							consulta = tmpA[i];
+							System.out.println("absent ox to BD:: " + consulta);
+							this.farm.getQuery().executeUpdate(consulta);
+						}
 
-					break;
-				default:
-					// sair
-					System.out.println("...");
-					this.wait(1000000);
-					finish = true;
-					System.out.println("FIM!");
-					System.exit(0);
-					break;
+						break;
+					default:
+						// sair
+						System.out.println("...");
+						this.wait(1000000);
+						finish = true;
+						System.out.println("FIM!");
+						System.exit(0);
+						break;
 				}
 
 				Graze tmpGraze = this.getFarm().getGrazes().get(this.getFarm().getGrazes().size() - 1);
@@ -159,14 +186,14 @@ public class PNApplication extends ModelApplication {
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
 
 	}
 
 	private void sendUnicastMessage(Vector<String> msg) {
-//	private void sendUnicastMessage(String msg) {
-//		System.out.println("UUID:\nHHHHHHHH-HHHH-HHHH-HHHH-HHHHHHHHHHHH");
+		// private void sendUnicastMessage(String msg) {
+		// System.out.println("UUID:\nHHHHHHHH-HHHH-HHHH-HHHH-HHHHHHHHHHHH");
 		try {
 			String messageText = "[REMETENTE]\t" + Constants.PN_UUID + "\n";
 			messageText = messageText + "[ACAO]tracking\n";
@@ -175,15 +202,16 @@ public class PNApplication extends ModelApplication {
 			Graze tmpGraze = new Graze();
 			messageText = messageText + "\n[TRACK_ID]\t" + tmpGraze.getId();
 			this.farm.setActualTrackingId(tmpGraze.getId());
-//			for (int i = 0; i < msg.size(); i++) {
-//				messageText = msg.get(i);
-//				System.out.print("Message: \n" + messageText);
-//				System.out.println(String.format("Sending |%s| to %s", messageText, Constants.GROUND_STATION_UUID));
-//			}
+			// for (int i = 0; i < msg.size(); i++) {
+			// messageText = msg.get(i);
+			// System.out.print("Message: \n" + messageText);
+			// System.out.println(String.format("Sending |%s| to %s", messageText,
+			// Constants.GROUND_STATION_UUID));
+			// }
 			this.farm.fileDrones(tmpGraze.getId());
 			this.farm.addGraze(tmpGraze);
-//			String consulta = "UPDATE farm SET notify = \'0\';";
-//			this.getFarm().getQuery().executeUpdate(tmpGraze.saveNewGraze());
+			// String consulta = "UPDATE farm SET notify = \'0\';";
+			// this.getFarm().getQuery().executeUpdate(tmpGraze.saveNewGraze());
 
 			sendRecord(createRecord("PrivateMessageTopic", Constants.GROUND_STATION_UUID,
 					swap.SwapDataSerialization(createSwapData(messageText))));
@@ -193,7 +221,7 @@ public class PNApplication extends ModelApplication {
 	}
 
 	public void recordReceived(ConsumerRecord arg0) {
-//		this.logger.debug("Record Received " + arg0.value().toString());
+		// this.logger.debug("Record Received " + arg0.value().toString());
 		if (this.farm != null) {
 			System.out.print("[PN] [receivedMessage] :\n");
 
@@ -202,76 +230,79 @@ public class PNApplication extends ModelApplication {
 				String msg = new String(data.getMessage(), StandardCharsets.UTF_8);
 				System.out.println("Mensagem recebida = \n" + msg);
 
-//				System.out.println(String.format("Mensagem recebida de %s", arg0.key()));
+				// System.out.println(String.format("Mensagem recebida de %s", arg0.key()));
 				String[] lines = msg.split("\n");
 
 				if (lines.length > 2) {
 					String sender = lines[0].substring(12);
 					String action = lines[1].substring(7);
-//				System.out.println("action: " + action);
+					// System.out.println("action: " + action);
 					switch (action) {
-					case "response":
-//						String track_id = lines[2].substring(11);
-						String track_id = this.farm.getActualTrackingId();
-//					System.out.println(track_id);
-//						if (track_id!=null && track_id.equalsIgnoreCase("null")) {
-//							System.out.println(lines[4].substring(7));
-//							track_id = this.getFarm().getGrazes().get(this.farm.getGrazes().size() - 1).getId();
-//							this.getFarm().getGrazes().get(this.farm.getGrazes().size() - 1).setEnd(null);
-//						}
-						boolean status = Boolean.parseBoolean(lines[3].substring(9));
-						String oxen = lines[4].substring(7);
-						String[] msgReceived = oxen.split("\t");
-//						System.out.println(msgReceived[0]);
-						Graze tmpGraze;
-						if (msgReceived != null && msgReceived.length >= 1) {
+						case "response":
+							// String track_id = lines[2].substring(11);
+							String track_id = this.farm.getActualTrackingId();
+							// System.out.println(track_id);
+							// if (track_id!=null && track_id.equalsIgnoreCase("null")) {
+							// System.out.println(lines[4].substring(7));
+							// track_id = this.getFarm().getGrazes().get(this.farm.getGrazes().size() -
+							// 1).getId();
+							// this.getFarm().getGrazes().get(this.farm.getGrazes().size() -
+							// 1).setEnd(null);
+							// }
+							boolean status = Boolean.parseBoolean(lines[3].substring(9));
+							String oxen = lines[4].substring(7);
+							String[] msgReceived = oxen.split("\t");
+							// System.out.println(msgReceived[0]);
+							Graze tmpGraze;
+							if (msgReceived != null && msgReceived.length >= 1) {
 
-//							if (this.getFarm() != null && !this.getFarm().hasGraze(track_id)) {
-//								System.out.println("não achou o ID : " + track_id);
-////							tmpGraze = new Graze();
-//								tmpGraze = this.getFarm().getGrazes().get(this.farm.getGrazes().size() - 1);
-//								tmpGraze.setId(track_id);
-////							this.getFarm().addGraze(tmpGraze);
-//							} 
-//							else {
+								// if (this.getFarm() != null && !this.getFarm().hasGraze(track_id)) {
+								// System.out.println("não achou o ID : " + track_id);
+								//// tmpGraze = new Graze();
+								// tmpGraze = this.getFarm().getGrazes().get(this.farm.getGrazes().size() - 1);
+								// tmpGraze.setId(track_id);
+								//// this.getFarm().addGraze(tmpGraze);
+								// }
+								// else {
 
-							tmpGraze = this.farm.getGrazesById(track_id);
+								tmpGraze = this.farm.getGrazesById(track_id);
 
-//							if (tmpGraze!=null) {
-//							System.out.println(tmpGraze.toString() + "\t id: "+track_id);
-//							}
-//							}
-							if (tmpGraze.getEnd() == null) {
+								// if (tmpGraze!=null) {
+								// System.out.println(tmpGraze.toString() + "\t id: "+track_id);
+								// }
+								// }
+								if (tmpGraze.getEnd() == null) {
 
-								for (int i = 0; i < msgReceived.length; i++) {
+									for (int i = 0; i < msgReceived.length; i++) {
 
-									if (!msgReceived[i].equalsIgnoreCase("Sem informações sobre rastreamento")) {
-//										System.out.println(i + "::ox received: " + msgReceived[i]);
-										if (!tmpGraze.hasPresentID(msgReceived[i])) {
-											tmpGraze.insertPresentID(msgReceived[i]);
-											this.getFarm().getQuery().executeUpdate(tmpGraze.insertPresentIDtoBD(msgReceived[i]));
+										if (!msgReceived[i].equalsIgnoreCase("Sem informações sobre rastreamento")) {
+											// System.out.println(i + "::ox received: " + msgReceived[i]);
+											if (!tmpGraze.hasPresentID(msgReceived[i])) {
+												tmpGraze.insertPresentID(msgReceived[i]);
+												this.getFarm().getQuery()
+														.executeUpdate(tmpGraze.insertPresentIDtoBD(msgReceived[i]));
+											}
+
+											// this.getFarm().insertOxIntoGraze(track_id, msgReceived[i]);
+											// this.farm.getQuery()
+											// .executeUpdate(tmpGraze.insertPresentIDtoBD(msgReceived[i]));
 										}
-
-//										this.getFarm().insertOxIntoGraze(track_id, msgReceived[i]);
-//										this.farm.getQuery()
-//												.executeUpdate(tmpGraze.insertPresentIDtoBD(msgReceived[i]));
 									}
+									// System.out.println(tmpGraze.updateGraze());
+									// this.farm.getQuery().executeUpdate(tmpGraze.updateGraze());
 								}
-//						System.out.println(tmpGraze.updateGraze());
-//								this.farm.getQuery().executeUpdate(tmpGraze.updateGraze());
+								System.out.println("Graze em andamento: " + tmpGraze.toString());
+								if (/* status || */(tmpGraze.getEnd() == null && (LocalDateTime.now().getMinute()
+										- tmpGraze.getStart().getMinute() >= Constants.GRAZE_TIMEOUT))) {
+									tmpGraze.setEnd(LocalDateTime.now());
+									this.farm.finishGrazeUpdate(tmpGraze);
+								}
 							}
-							System.out.println("Graze em andamento: " + tmpGraze.toString());
-							if (/*status || */(tmpGraze.getEnd() == null && (LocalDateTime.now().getMinute()
-									- tmpGraze.getStart().getMinute() >= Constants.GRAZE_TIMEOUT))) {
-								tmpGraze.setEnd(LocalDateTime.now());
-								this.farm.finishGrazeUpdate(tmpGraze);
-							}
-						}
-						break;
-					case "alive":
-						break;
-					default:
-						break;
+							break;
+						case "alive":
+							break;
+						default:
+							break;
 					}
 				}
 			} catch (Exception e) {
--- hmt-pn-application/src/icmu/util/connection/DataBaseConnection.java
+++ hmt-pn-application/src/icmu/util/connection/DataBaseConnection.java
@@ -18,41 +18,14 @@ public class DataBaseConnection {
 	}
 
 	public static void main(String[] argvs) {
-//		DataBaseConnection bd = new DataBaseConnection();
+		// DataBaseConnection bd = new DataBaseConnection();
 
 		try {
 			Connection conn = DataBaseConnection.getDataBaseConnection();
 			Statement query = conn.createStatement();
-//			String consulta = "SELECT * FROM ox";
-//			String consulta = "INSERT INTO boi VALUES(4,\'lala\');";
-//			String consulta = "INSERT INTO farm (`name`, `description`) VALUES(\'teste_farm3\',\'exemplo de descrição\');";
-//			INSERT INTO `farm` (`name`, `description`) VALUES ('FarmMI', 'Minha fazenda');
-//			String consulta = "UPDATE boi SET nome=\'maisum\' WHERE id=4;";
-//			System.out.println("Query: " + consulta);
-
-//			Graze tmp = new Graze();
-//			String consulta = tmp.saveNewGraze();
-//			System.out.println(consulta);
-//			int inserir = query.executeUpdate(consulta);
-//			
-//			tmp.insertPresentID("1b758239-be85-4695-b743-b2dc294115ca");
-//			tmp.insertPresentID("2d98c245-0eab-472d-90e1-26fd347d9641");
-//			tmp.insertPresentID("3c8a3ffc-07d2-4ea2-ae98-aa99350d63c7");
-//			tmp.insertPresentID("72cc84f6-89c8-48b6-ac58-56df1d565cd3");
-//			tmp.insertPresentID("bcda7cd1-c9f6-4b53-a676-91ad4bc0b58a");
-//			tmp.setEnd(LocalDateTime.now());
-//			
-//			consulta = tmp.updateGraze();
-//			inserir = query.executeUpdate(consulta);
-//			
-//			consulta = tmp.insertPresents();
-//			inserir = query.executeUpdate(consulta);;
-//			consulta = tmp.insertAbsents();
-//			inserir = query.executeUpdate(consulta);;
-//			int inserir = query.executeUpdate(consulta);
 
 			String consulta = "SELECT * from ox";
-			
+
 			ResultSet resultado = query.executeQuery(consulta);
 			while (resultado.next()) {
 
@@ -72,25 +45,28 @@ public class DataBaseConnection {
 
 		try {
 
-// Carregando o JDBC Driver padrão
+			// Carregando o JDBC Driver padrão
 
-//			String driverName = "com.mysql.jdbc.Driver";
+			// String driverName = "com.mysql.jdbc.Driver";
 			String driverName = "com.mysql.cj.jdbc.Driver";
 
 			Class.forName(driverName);
 
-// Configurando a nossa conexão com um banco de dados//
+			// Configurando a nossa conexão com um banco de dados//
 
-//			String serverName = "192.168.100.196"; // caminho do servidor do BD
-//			String mydatabase = "hmt"; // nome do seu banco de dados
-//			String url = "jdbc:mysql://" + serverName + "/" + mydatabase + "?useSSL=false";
-//			String url = "jdbc:mysql://" + Constants.SERVER_NAME + "/" + Constants.DATABASE + "?useSSL=false";
-			String url = "jdbc:mysql://45.56.126.161:3306/" + Constants.DATABASE + "?useSSL=false&allowPublicKeyRetrieval=true";
-			
-//			jdbc:mysql://ip-do-servidor:porta/banco-de-dados?useSSL=false
+			// String serverName = "192.168.100.196"; // caminho do servidor do BD
+			// String mydatabase = "hmt"; // nome do seu banco de dados
+			// String url = "jdbc:mysql://" + serverName + "/" + mydatabase +
+			// "?useSSL=false";
+			// String url = "jdbc:mysql://" + Constants.SERVER_NAME + "/" +
+			// Constants.DATABASE + "?useSSL=false";
+			String url = Constants.getMYSQL_ADDRESS() + Constants.DATABASE
+					+ "?useSSL=false&allowPublicKeyRetrieval=true";
 
-//			String username = "millena"; // nome de um usuário de seu BD
-//			String password = "Spoilt69"; // sua senha de acesso
+			// jdbc:mysql://ip-do-servidor:porta/banco-de-dados?useSSL=false
+
+			// String username = "millena"; // nome de um usuário de seu BD
+			// String password = "Spoilt69"; // sua senha de acesso
 			System.out.println("antes");
 			connection = DriverManager.getConnection(url, Constants.USERNAME, Constants.PASSWD);
 			System.out.println("depois");
@@ -117,7 +93,7 @@ public class DataBaseConnection {
 
 		} catch (SQLException e) {
 
-//Não conseguindo se conectar ao banco
+			// Não conseguindo se conectar ao banco
 			e.printStackTrace();
 			System.out.println("Nao foi possivel conectar ao Banco de Dados.");
 
